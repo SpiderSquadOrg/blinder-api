6b8e0752b822e4a7b8e5d29c9594d6b138438c5a
Yunus Emre Yekta <yeyfb2001@gmail.com>
2023-12-08 19:36:37 +0300

    Hobby and Book(?) implemented.

--- src/main/java/com/blinder/api/book/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blinder.api.book.repository;
-
-import com.blinder.api.book.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
