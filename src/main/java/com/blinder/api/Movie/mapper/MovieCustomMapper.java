d798b5df1c82537c616899c58845524c7876af37
Yunus Emre Yekta <yeyfb2001@gmail.com>
2023-12-09 01:39:15 +0300

    Movie implemented.

--- src/main/java/com/blinder/api/Movie/mapper/MovieCustomMapper.java
+++ src/main/java/com/blinder/api/Movie/mapper/MovieCustomMapper.java
@@ -2,14 +2,40 @@ package com.blinder.api.Movie.mapper;
 
 import com.blinder.api.Movie.dto.MovieResponseDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class MovieCustomMapper {
     public List<MovieResponseDto> movieDataToMovieResponseDto(Mono<String> movieData) throws JsonProcessingException {
-        return null;
+        List<MovieResponseDto> movieResponseDtos = new ArrayList<>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = movieData.block();
+
+        JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+        JsonNode items = jsonNode.get("results");
+
+        items.forEach((item) -> {
+            MovieResponseDto movieResponseDto = new MovieResponseDto();
+            movieResponseDto.setImdbId(item.get("_id").asText());
+            movieResponseDto.setName(item.get("titleText").get("text").asText());
+            try{
+                movieResponseDto.setYear(item.get("releaseYear").get("year").asInt());
+                movieResponseDto.setImage(item.get("primaryImage").get("url").asText());
+                movieResponseDtos.add(movieResponseDto);
+            }catch (NullPointerException ignored){
+
+            }
+        });
+
+        return movieResponseDtos;
+
     }
 }
--- src/main/java/com/blinder/api/MusicCategory/dto/CreateMusicCategoryRequestDto.java
+++ src/main/java/com/blinder/api/MusicCategory/dto/CreateMusicCategoryRequestDto.java
@@ -1,7 +1,6 @@
 package com.blinder.api.MusicCategory.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
