faeb2bc0ab85920289611b22b8ab0b3056cc40c9
Batuhan Dikmen <rbdikmen@gmail.com>
2023-12-16 04:44:14 +0300

    Some errors which caused by CAĞLA are fixed FINALLY

--- src/main/java/com/blinder/api/possibleMatch/dto/PossibleMatchResponseDto.java
+++ src/main/java/com/blinder/api/possibleMatch/dto/PossibleMatchResponseDto.java
@@ -1,6 +1,7 @@
 package com.blinder.api.possibleMatch.dto;
 
 import com.blinder.api.possibleMatch.model.PossibleMatchStatus;
+import com.blinder.api.user.dto.UserResponseDto;
 import com.blinder.api.user.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,8 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PossibleMatchResponseDto {
     private String id;
-    private String to;
-    private String from;
+    //private String to;
+    //private String from;
+    private UserResponseDto to;
+    private UserResponseDto from;
     private double similarityScore;
     private PossibleMatchStatus status;
 }
