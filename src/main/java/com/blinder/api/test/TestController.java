72a25f1661387df910e1ce49b5637f0807e8384b
Batuhan Dikmen <rbdikmen@gmail.com>
2024-01-06 14:00:02 +0300

    Test

---
+++ src/main/java/com/blinder/api/test/TestController.java
@@ -0,0 +1,20 @@
+package com.blinder.api.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return new ResponseEntity<>("Running", HttpStatus.OK);
+    }
+}
+
